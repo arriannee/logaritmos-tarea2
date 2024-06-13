e78e44391d6ca7a27ed04e067fbc42cf63938630
Sebastián Alonso Burgueño Merino <sebastian.burgueno@ug.uchile.cl>
2024-06-13 01:14:53 -0400

    Heap to csv
    Se agrega carpeta heaptocsv que contiene código para crear archivos csv para cada 2^i, con el fin de poner hacer plots con estos.

---
+++ heap_csv/heaptocsv.py
@@ -0,0 +1,65 @@
+#Para v = 2^10
+
+# Lee el archivo tiempos_dijkstra_heap.txt
+with open('tiempos_dijkstra_heap.txt', 'r') as file:
+    lines = file.readlines()
+
+# Abre el archivo CSV para escribir
+with open('tiempos_heap_2^10.csv', 'w') as csv_file:
+    # Escribe la cabecera opcional si la deseas
+    # csv_file.write('E, T\n')
+
+    # Recorre cada línea del archivo original
+    for i, line in enumerate(lines[:150]):
+        # Divide la línea en sus componentes
+        parts = line.split()
+        # Obtiene E y T según la estructura conocida
+        aristasAux = parts[9][:-1]  # parts[10] corresponde a aristas
+        aristas = int(aristasAux[0]) ** int(aristasAux[2]+aristasAux[3]) # Se eleva 2 al exponente correspondiente, 16,...,18
+        tiempo = parts[10]  # parts[6] corresponde a tiempo  
+        # Escribe la línea en el archivo CSV
+        csv_file.write(f'{aristas}, {tiempo}\n')
+
+print("Archivo tiempos_heap_2^10.csv generado exitosamente.")
+
+#Para v = 2^12
+
+# Abre el archivo CSV para escribir
+with open('tiempos_heap_2^12.csv', 'w') as csv_file:
+    # Escribe la cabecera opcional si la deseas
+    # csv_file.write('E, T\n')
+
+    # Recorre cada línea del archivo original
+    for i, line in enumerate(lines[150:500]):
+        # Divide la línea en sus componentes
+        parts = line.split()
+        # Obtiene E y T según la estructura conocida
+        aristasAux = parts[9][:-1]  # parts[10] corresponde a aristas
+        aristas = int(aristasAux[0]) ** int(aristasAux[2]+aristasAux[3]) # Se eleva 2 al exponente correspondiente, 16,...,22
+        tiempo = parts[10]  # parts[6] corresponde a tiempo
+
+        # Escribe la línea en el archivo CSV
+        csv_file.write(f'{aristas}, {tiempo}\n')
+
+print("Archivo tiempos_heap_2^12.csv generado exitosamente.")
+
+# Para v = 2^14
+
+# Abre el archivo CSV para escribir
+with open('tiempos_heap_2^14.csv', 'w') as csv_file:
+    # Escribe la cabecera opcional si la deseas
+    # csv_file.write('E, T\n')
+
+    # Recorre cada línea del archivo original
+    for i, line in enumerate(lines[500:850]):
+        # Divide la línea en sus componentes
+        parts = line.split()
+        # Obtiene E y T según la estructura conocida
+        aristasAux = parts[9][:-1]  # parts[10] corresponde a aristas
+        aristas = int(aristasAux[0]) ** int(aristasAux[2]+aristasAux[3]) # Se eleva 2 al exponente correspondiente, 16,...,22
+        tiempo = parts[10]  # parts[6] corresponde a tiempo
+
+        # Escribe la línea en el archivo CSV
+        csv_file.write(f'{aristas}, {tiempo}\n')
+
+print("Archivo tiempos_heap_2^14.csv generado exitosamente.")
